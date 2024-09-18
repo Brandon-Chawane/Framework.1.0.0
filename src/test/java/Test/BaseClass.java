5615b197e008f8077fc163a2aacf9c28422ba514
Brandon Chawane <brandon.chawane@gmail.com>
2024-09-18 12:42:02 +0200

    First commit

---
+++ src/test/java/Test/BaseClass.java
@@ -0,0 +1,46 @@
+package Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
+public class BaseClass {
+    
+    WebDriver driver;
+    public Logger logger;
+    @BeforeClass
+    @Parameters({"browser","url"})
+    public void setup(String browser, String url ){
+
+        logger = LogManager.getLogger(this.getClass());
+
+        switch (browser.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/Test/TC001.java
@@ -0,0 +1,6 @@
+package Test;
+
+public class TC001 extends BaseClass{
+
+
+}
